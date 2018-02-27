88ce9354d2acd14bbf6d66f6bdb2f3800b56202b
Alejandro Ravelo Julian <alejulian9119@gmail.com>
2018-02-27 15:55:27 +0800

    adding script

--- aframe/src/examples.js
+++ aframe/src/examples.js
@@ -1,12 +1,23 @@
-AFRAME.registerComponent('press_click', {
-    schema: {
-        color: {default: 'red'}
-    },
+AFRAME.registerComponent('press', {
+    schema: {type: 'number'},
 
     init: function () {
-        document.querySelector('a-entity').addEventListener('click', function (evt) {
-            console.log('This 2D element was clicked!');
-        });
+        var stringToLog = this.data;
+        console.log(stringToLog);
+
+        var a = document.querySelector('a');
+        console.log(a);
+    },
+
+    update: function () {
+        // Do something when component's data is updated.
+    },
+
+    remove: function () {
+        // Do something the component or its entity is detached.
+    },
+
+    tick: function (time, timeDelta) {
+        // Do something on every scene tick or frame.
     }
 });
-
