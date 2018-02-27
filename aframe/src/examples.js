2d09f88ea5932a0c1d7d35e34bee571f30776786
Alejandro Ravelo Julian <alejulian9119@gmail.com>
2018-02-27 13:28:26 +0800

    adding script

--- aframe/src/examples.js
+++ aframe/src/examples.js
@@ -8,12 +8,8 @@ AFRAME.registerComponent('change-color-on-hover', {
         var el = this.el;  // <a-box>
         var defaultColor = el.getAttribute('material').color;
 
-        el.addEventListener('mouseenter', function () {
-            el.setAttribute('color', data.color);
-        });
-
-        el.addEventListener('mouseleave', function () {
-            el.setAttribute('color', defaultColor);
+        el.addEventListener('click', function () {
+            console.log("click al objeto");
         });
     }
 });
