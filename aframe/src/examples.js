65c3a08a33a1f7c5592f45a987d757caf764eeb4
Alejandro Ravelo Julian <alejulian9119@gmail.com>
2018-02-27 14:31:30 +0800

    adding script

--- aframe/src/examples.js
+++ aframe/src/examples.js
@@ -4,20 +4,9 @@ AFRAME.registerComponent('press_click', {
     },
 
     init: function () {
-        var data = this.data;
-        var el = this.el;  // <a-box>
-        var defaultColor = el.getAttribute('material').color;
-
-        el.addEventListener('mouseenter', function () {
-            el.setAttribute('color', data.color);
-        });
-
-        el.addEventListener('mouseleave', function () {
-            el.setAttribute('color', defaultColor);
+        document.querySelector('a-entity').addEventListener('click', function (evt) {
+            console.log('This 2D element was clicked!');
         });
     }
 });
 
-document.querySelector('a-box').addEventListener('click', function (evt) {
-    console.log('This 2D element was clicked!');
-});
