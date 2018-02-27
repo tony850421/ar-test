c7c102d45dc1bd93837af9af07311c233126bf2a
Alejandro Ravelo Julian <alejulian9119@gmail.com>
2018-02-27 16:53:58 +0800

    adding script

--- aframe/src/examples.js
+++ aframe/src/examples.js
@@ -2,24 +2,3 @@ function pressButton() {
     var box = document.getElementById('boxExample');
     box.setAttribute('position', '0 0 -1');
 }
-
-AFRAME.registerComponent('press', 
-    schema: {type: 'number'},
-
-    init: function () {
-        var stringToLog = this.data;
-        console.log(stringToLog);
-    },
-
-    update: function () {
-        // Do something when component's data is updated.
-    },
-
-    remove: function () {
-        // Do something the component or its entity is detached.
-    },
-
-    tick: function (time, timeDelta) {
-        // Do something on every scene tick or frame.
-    }
-});
