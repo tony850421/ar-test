2631d348d659e7902fe698839a4597fe760841b2
Alejandro Ravelo Julian <alejulian9119@gmail.com>
2018-02-28 11:47:03 +0800

    adding range input

--- aframe/src/examples.js
+++ aframe/src/examples.js
@@ -3,10 +3,18 @@ function pressButton() {
     box.setAttribute('position', '1 0 -1');
 }
 
-function scroll() {
+function HorizontalMove() {
     var scrollH = document.getElementById('horizontal').value;
-    var scrollV = document.getElementById('vertical').value;
+    console.log(scrollH + " value H");
 
     var box = document.getElementById('monkeyObject');
-    box.setAttribute('position', {x:scrollH, y:scrollV, z:0});
+    box.setAttribute('position', {x:scrollH});
+}
+
+function VerticalMove() {
+    var scrollV = document.getElementById('vertical').value;
+    console.log(scrollV + " value H");
+
+    var box = document.getElementById('monkeyObject');
+    box.setAttribute('position', {y:scrollV});
 }
